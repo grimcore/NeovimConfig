54ffbea950615ea5fcc65abcab34ce8b2f512bb8
lucathehun <luca@gatty.dev>
2024-08-19 09:49:27 +0200

    update

--- init.lua
+++ init.lua
@@ -125,5 +125,27 @@ prettier.setup({
   },
 })
 
+require('render-markdown').setup({
+    link = {
+        -- Turn on / off inline link icon rendering
+        enabled = true,
+        -- Inlined with 'image' elements
+        image = '󰥶 ',
+        -- Fallback icon for 'inline_link' elements
+        hyperlink = '󰌹 ',
+        -- Applies to the fallback inlined icon
+        highlight = 'RenderMarkdownLink',
+        -- Define custom destination patterns so icons can quickly inform you of what a link
+        -- contains. Applies to 'inline_link' and wikilink nodes.
+        -- Can specify as many additional values as you like following the 'web' pattern below
+        --   The key in this case 'web' is for healthcheck and to allow users to change its values
+        --   'pattern':   Matched against the destination text see :h lua-pattern
+        --   'icon':      Gets inlined before the link text
+        --   'highlight': Highlight for the 'icon'
+        custom = {
+            web = { pattern = '^http[s]?://', icon = '󰖟 ', highlight = 'RenderMarkdownLink' },
+        },
+    },
+})
 -- display relative line numbers 
 vim.opt.relativenumber = true
