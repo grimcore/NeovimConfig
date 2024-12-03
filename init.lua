322f34d837b024c9de1448f7d21a507f43a36a92
lucathehun <luca@gatty.dev>
2024-12-03 08:48:41 +0100

    update

--- init.lua
+++ init.lua
@@ -159,10 +159,26 @@ autocmd("BufReadPost", {
     end,
 })
 
-------< info :h nvim_set_hl >------
-vim.api.nvim_set_hl(0, "comment", {
-    fg = "#ffffff",
-    bg = "#bd93f9",
-    italic = true,
-    bold = true,
-})
+-- ------< info :h nvim_set_hl >------
+-- vim.api.nvim_set_hl(0, "comment", {
+--     fg = "#ffffff",
+--     bg = "#bd93f9",
+--     italic = true,
+--     bold = true,
+-- })
+
+------< todo comments >------
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- You can also specify a list of valid jump keywords
+
+-- vim.keymap.set("n", "]t", function()
+--   require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+-- end, { desc = "Next error/warning todo comment" })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,15 +50,15 @@ return {
     },
 
     {
+        "nvim-lua/plenary.nvim",
+    },
+
+    {
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
             require("null-ls").setup()
         end,
         requires = { "nvim-lua/plenary.nvim" },
-    },
-
-    {
-        "nvim-lua/plenary.nvim",
     },
 
     {
@@ -163,5 +163,27 @@ return {
 
             return conf
         end,
+    },
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = false,
+        opts = {
+            --     keywords = {
+            --         FIX = {
+            --             icon = " ", -- icon used for the sign, and in search results
+            --             color = "error", -- can be a hex color, or a named color (see below)
+            --             alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            --             -- signs = false, -- configure signs for some keywords individually
+            --         },
+            --         TODO = { icon = " ", color = "info" },
+            --         HACK = { icon = " ", color = "warning" },
+            --         WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+            --         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            --         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+            --         TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+            --     },
+        },
     },
 }
