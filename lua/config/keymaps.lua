e4a612b7d45a517a07b00ac16c85e3e509a391fd
lucathehun <luca@gatty.dev>
2025-04-09 01:38:31 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,30 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- enter visual mode with jk kj JK KJ
-vim.keymap.set({ "i" }, "jk", "<ESC>", { silent = true, noremap = true })
-vim.keymap.set({ "i" }, "kj", "<ESC>", { silent = true, noremap = true })
+local map = vim.keymap.set
 
--- cursor is always centered with jzz kzz in visual normal and insert mode
-vim.keymap.set({ "n", "v" }, "j", "jzz", { silent = true, noremap = true })
-vim.keymap.set({ "n", "v" }, "k", "kzz", { silent = true, noremap = true })
+map({ "n", "v" }, "j", "gjzz")
+map({ "n", "v" }, "k", "gkzz")
+map("i", "<c-h>", "<left>")
+map("i", "<c-j>", "<down>")
+map("i", "<c-k>", "<up>")
+map("i", "<c-l>", "<right>")
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
+
+-- save and quit
+map({ "n", "v" }, "<leader>q", ":q<CR>")
+map({ "n", "v" }, "<leader>qa", ":qa<CR>")
+map({ "n", "v" }, "<leader>wq", ":wq<CR>")
+map({ "n", "v" }, "<leader>w", ":w<CR>")
+map({ "n", "v" }, "<leader>W", ":w!<CR>")
+map({ "n", "v" }, "<leader>Q", ":q!<CR>")
+
+-- pick color theme with telescope implementation using something like telescope.nvim
+map(
+  "n",
+  "<leader>th",
+  "<cmd>lua require('telescope.builtin').colorscheme({enable_preview=true})<cr>",
+  { noremap = true, silent = true }
+)
